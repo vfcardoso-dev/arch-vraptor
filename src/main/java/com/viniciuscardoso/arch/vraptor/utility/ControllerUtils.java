f02ed5c53dda65ba0e3dd06919f884fff5a723a3
Vinicius Cardoso <vfcardoso3@gmail.com>
2014-11-13 12:01:40 -0200

    Melhoria no tratamento de exceções.

--- src/main/java/com/viniciuscardoso/arch/vraptor/utility/ControllerUtils.java
+++ src/main/java/com/viniciuscardoso/arch/vraptor/utility/ControllerUtils.java
@@ -58,7 +58,7 @@ public class ControllerUtils {
      * @param e Exception lançada
      */
     public static void defineErrorMessage(Result result, Exception e) {
-        result.include("alerta", e.getMessage());
+        result.include("alerta", (e.getMessage() == null || e.getMessage().equals("")) ? "NullPointerException" : e.getMessage());
         if (e.getCause() != null) result.include("causa", " Causa: " + e.getCause());
         result.include("classeAlerta", "alert-danger");
     }
