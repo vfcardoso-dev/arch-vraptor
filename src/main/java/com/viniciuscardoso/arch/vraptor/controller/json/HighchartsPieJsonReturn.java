c81eeec08e821fa7749acbe890cae73099b031b0
Vinicius Cardoso <vfcardoso3@gmail.com>
2015-03-05 15:36:47 -0300

    Classe wrapper para gráfico de pizza

---
+++ src/main/java/com/viniciuscardoso/arch/vraptor/controller/json/HighchartsPieJsonReturn.java
@@ -0,0 +1,31 @@
+package com.viniciuscardoso.arch.vraptor.controller.json;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Project: arch-vraptor
+ * User: Vinícius
+ * Date: 05/03/2015
+ * Time: 15:35
+ */
+public class HighchartsPieJsonReturn {
+    private String name;
+    private Map<String, Double> mapa = new HashMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Double> getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(Map<String, Double> mapa) {
+        this.mapa = mapa;
+    }
+}
