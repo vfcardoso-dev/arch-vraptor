390dbdd1013aa65f3ee217165c63419fbac44880
Vinicius Cardoso <vfcardoso3@gmail.com>
2015-08-28 09:55:14 -0300

    Correção na automatização da inclusão dos parâmetros na consulta JqGrid em db firebird.

--- src/main/java/com/viniciuscardoso/arch/vraptor/dao/helpers/JqGridQueryBuilderFirebird.java
+++ src/main/java/com/viniciuscardoso/arch/vraptor/dao/helpers/JqGridQueryBuilderFirebird.java
@@ -4,6 +4,7 @@ import br.com.caelum.vraptor.ioc.Component;
 import com.viniciuscardoso.arch.vraptor.controller.json.JqGridFilters;
 import com.viniciuscardoso.arch.vraptor.controller.json.JqGridRules;
 import org.hibernate.Query;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -37,14 +38,15 @@ public class JqGridQueryBuilderFirebird extends JqGridQueryBuilder implements IJ
             consulta.append("and ").append(sField).append(this.convertSearchOper(sOper)).append(" :param ");
         } else if (filters != null && filters.getRules().size() > 0) {
             JqGridRules rule;
-            consulta.append("and ");
             for (int i = 0; i < filters.getRules().size(); i++) {
                 rule = filters.getRules().get(i);
-                consulta.append(rule.getField())
-                        .append(convertSearchOper(rule.getOp()))
-                        .append(" :param").append(String.valueOf(i)).append(" ");
-                if (i < filters.getRules().size() - 1) {
-                    consulta.append(" ").append(filters.getGroupOp()).append(" ");
+                if (rule.getField().toLowerCase().contains("dt") && StringUtils.countOccurrencesOf(rule.getData(),"/") == 2 && rule.getData().length() > 7) {
+                    consulta.append("and ").append(rule.getField())
+                            .append(convertSearchOper(rule.getOp()))
+                            .append(" :param").append(String.valueOf(i)).append(" ");
+                    if (i < filters.getRules().size() - 1) {
+                        consulta.append(" ").append(filters.getGroupOp()).append(" ");
+                    }
                 }
             }
         }
@@ -87,7 +89,12 @@ public class JqGridQueryBuilderFirebird extends JqGridQueryBuilder implements IJ
             for (int i = 0; i < filters.getRules().size(); i++) {
                 rule = filters.getRules().get(i);
                 String paramIdx = "param" + String.valueOf(i);
-                if ("cn".equals(rule.getOp())) {
+                if (rule.getField().toLowerCase().contains("dt")) {
+                    if (StringUtils.countOccurrencesOf(rule.getData(), "/") == 2 && rule.getData().length() > 7) { // '00/00/'+
+                        String data = rule.getData().split("/")[2] + "-" + rule.getData().split("/")[1] + "-" + rule.getData().split("/")[0];
+                        q.setParameter(paramIdx, data);
+                    }
+                } else if ("cn".equals(rule.getOp())) {
                     q.setParameter(paramIdx, rule.getData());
                 } else {
                     super.addCustomParametersToQuery(q, rule.getField(), rule.getData(), rule.getOp());
