8cc63f370ac822bfdeac75782352e8ad65e044fe
Vinicius Cardoso <vfcardoso3@gmail.com>
2016-02-02 13:46:01 -0200

    Mais correções no parser de HTML pra pdf

--- src/main/java/com/viniciuscardoso/arch/vraptor/utility/ExportUtils.java
+++ src/main/java/com/viniciuscardoso/arch/vraptor/utility/ExportUtils.java
@@ -13,20 +13,24 @@ import java.io.*;
  * Date: 28/03/14
  * Time: 16:02
  */
+
 public class ExportUtils {
 
     public static void html2pdf(String input, OutputStream out) throws DocumentException, IOException {
         html2pdf(new ByteArrayInputStream(input.getBytes()), out);
     }
 
+    public static void html2pdf(org.jsoup.nodes.Document doc, OutputStream out) throws DocumentException, IOException {
+        html2pdf(new ByteArrayInputStream(doc.html().getBytes()), out);
+    }
+
+    @SuppressWarnings("deprecation")
     public static void html2pdf(InputStream input, OutputStream out) throws DocumentException, IOException {
         Document document = new Document();
         PdfWriter.getInstance(document, out);
         document.open();
-
         HTMLWorker hw = new HTMLWorker(document);
         hw.parse(new InputStreamReader(input));
-        //XMLWorkerHelper.getInstance().parseXHtml(writer, document, input);
         document.close();
     }
 }
