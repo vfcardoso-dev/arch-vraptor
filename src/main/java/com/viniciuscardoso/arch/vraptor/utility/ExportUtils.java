79fb84c07d5d2b5ab308a7299b5db31d0af7855d
Vinicius Cardoso <vfcardoso3@gmail.com>
2016-02-02 12:43:29 -0200

    Adição de método de conversão de XHTML para PDF

--- src/main/java/com/viniciuscardoso/arch/vraptor/utility/ExportUtils.java
+++ src/main/java/com/viniciuscardoso/arch/vraptor/utility/ExportUtils.java
@@ -1,5 +1,15 @@
 package com.viniciuscardoso.arch.vraptor.utility;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Project: arch
  * User: Vinicius Cardoso
@@ -7,4 +17,17 @@ package com.viniciuscardoso.arch.vraptor.utility;
  * Time: 16:02
  */
 public class ExportUtils {
+
+    public static void html2pdf(String input, OutputStream out) throws DocumentException, IOException {
+        html2pdf(new ByteArrayInputStream(input.getBytes()), out);
+    }
+
+    public static void html2pdf(InputStream input, OutputStream out) throws DocumentException, IOException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, out);
+        document.open();
+
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document, input);
+        document.close();
+    }
 }
