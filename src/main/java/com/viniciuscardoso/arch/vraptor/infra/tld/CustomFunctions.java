81cf98a4f9fe54654a174ee8a1a98985381e25f6
Vinícius Cardoso <vfcardoso3@gmail.com>
2016-08-25 10:08:06 -0300

    Adicionando método localizeNumber na classe tld.CustomFunctions

--- src/main/java/com/viniciuscardoso/arch/vraptor/infra/tld/CustomFunctions.java
+++ src/main/java/com/viniciuscardoso/arch/vraptor/infra/tld/CustomFunctions.java
@@ -1,6 +1,9 @@
 package com.viniciuscardoso.arch.vraptor.infra.tld;
 
+import com.viniciuscardoso.arch.vraptor.utility.ConvertUtils;
+
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -23,5 +26,10 @@ public class CustomFunctions {
         } else {
             return false;
         }
+    }
+
+    public static String localizeNumber(Double number, int decimals, Locale locale) {
+        locale = locale == null ? new Locale("pt", "BR") : locale;
+        return ConvertUtils.getFormattedNumber(number, decimals, locale);
     }
 }
